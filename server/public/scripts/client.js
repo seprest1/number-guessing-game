7b4a7f71c18dbb2b4718a53a67641d3bdc242807
Ryan Hess <ryanmhess@gmail.com>
2022-09-01 14:29:25 -0500

    90% basemode complete, working on new game button and winner display event." git status

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,49 @@
-$(document).ready(handleReady);
+$(document).ready(onReady);
 
-function handleReady() {
+function onReady() {
   console.log("jquery is loaded!")
+  $('#submit-btn').on('click', sendPlayerGuess);
+  $('#newGame-btn').on('click', newGamePlease);
+}
+
+//  SENDING Player Guess to SERVER
+function sendPlayerGuess() {
+  let playerOne = $('#playerOne').val();
+  let playerTwo = $('#playerTwo').val();
+  let playerThree = $('#playerThree').val();
+  $.ajax( { 
+    method: 'POST',
+    url: '/checkguess',
+    data: {playerOne, playerTwo, playerThree}
+  }).then(function(gameResponse) {
+    console.log(gameResponse);
+    fetchGameHistory();
+    $('#playerOne').val('');
+    $('#playerTwo').val('');
+    $('#playerThree').val('');
+  })
+}
+
+//  GETTING Round Data from SERVER 
+function fetchGameHistory() {
+  $.ajax( {
+    type: 'GET',
+    url: '/checkguess'
+  }).then(function(historyResponse) {
+    $('#tableBody').empty();
+    for (let value of historyResponse) {
+      $('#tableBody').prepend(`
+        <tr>
+          <td>${value.roundCount}</td>
+          <td>${value.playerOne}</td>
+          <td>${value.playerTwo}</td>
+          <td>${value.playerThree}</td>
+        </tr>
+      `)
+    }
+  })
+}
+
+function newGamePlease() {
+
 }
--- server/server.js
+++ server/server.js
@@ -11,7 +11,83 @@ app.use(express.static('server/public'));
 
 // GET & POST Routes go here
 
+let randomNumber;
+let roundCount = 0;
+let playerGuessEvaluation = [];
+let maxNum = 25;
+
+//  RECEIVING Player Guess from CLIENT
+app.post('/checkguess', (req, res) => {
+  roundCount++;
+  if (roundCount === 1) {
+    randomNumberGen();
+  }
+  console.log(randomNumber);
+  console.log('post request sent. req.body is ', req.body)
+  //gives req.body a name we can understand
+  let playerGuesses = req.body;
+  //adding roundCount to body object
+  playerGuesses.roundCount = roundCount;
+
+  //sends player guess object to checkPlayerGuesses
+  checkPlayerGuesses(playerGuesses);
+  //response: success! (2xx==success)
+  res.sendStatus(201);
+});
+
+//  SENDING Round Data to CLIENT
+app.get('/checkguess', (req, res) => { //route names... should these be the same or different?
+  console.log('get request sent');
+  //sends object of player evaluated player guesses
+    res.send(playerGuessEvaluation);
+});
 
 app.listen(PORT, () => {
-  console.log ('Server is running on port', PORT)
+  console.log ('Server is running on port', PORT, 'http://localhost:5000/')
 })
+
+function randomNumberGen() {
+  randomNumber = (Math.floor(Math.random() * maxNum ))+1;
+  console.log(randomNumber);
+}
+
+function checkPlayerGuesses(guessObject) {
+  console.log(guessObject);
+// compares player guesses to randomNumber
+//playerOne
+if (guessObject.playerOne === String(randomNumber)) {
+  guessObject.playerOne = 'match';
+} else if (guessObject.playerOne < randomNumber) {
+  guessObject.playerOne = 'low';
+} else if (guessObject.playerOne > randomNumber) {
+  guessObject.playerOne = 'high';
+}
+//playerTwo
+if (guessObject.playerTwo === String(randomNumber)) {
+  guessObject.playerTwo = 'match';
+} else if (guessObject.playerTwo < randomNumber) {
+  guessObject.playerTwo = 'low';
+} else if (guessObject.playerTwo > randomNumber) {
+  guessObject.playerTwo = 'high';
+}
+//playerThree
+if (guessObject.playerThree === String(randomNumber)) {
+  guessObject.playerThree = 'match';
+} else if (guessObject.playerThree < randomNumber) {
+  guessObject.playerThree = 'low';
+} else if (guessObject.playerThree > randomNumber) {
+  guessObject.playerThree = 'high';
+}
+// for each player object:
+//  compare guess to random number.
+//  give value: High, Low, Match
+//  output to playerGuessEvaluation:
+//    {
+//      { player, guessValue },
+//      {player, guessValue},
+//    }
+  //pushes body object to player guess history array
+  console.log(guessObject);
+  playerGuessEvaluation.push(guessObject);
+  console.log(playerGuessEvaluation);
+}
